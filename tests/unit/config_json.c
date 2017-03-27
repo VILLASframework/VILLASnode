8341f6657556a71d233a0cb4ad010b93a94ca06d
Steffen Vogel <post@steffenvogel.de>
2017-03-27 12:38:19 +0200

    added json_to_config in order to support JSON as new config file format

---
+++ tests/unit/config_json.c
@@ -0,0 +1,116 @@
+/** Unit tests libconfig to jansson converters.
+ *
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @copyright 2017, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU Lesser General Public License v2.1
+ *
+ * VILLASnode - connecting real-time simulation equipment
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *********************************************************************************/
+
+#include <criterion/criterion.h>
+
+#include <jansson.h>
+#include <libconfig.h>
+
+#include "utils.h"
+
+const char *cfg_example = "test : \n"
+			  "{\n"
+			  "  hallo = 1L;\n"
+			  "};\n"
+			  "liste = ( 1.1, 2L, 3L, 4L, \n"
+			  "  {\n"
+			  "    objekt : \n"
+			  "    {\n"
+			  "      key = \"value\";\n"
+			  "    };\n"
+			  "  } );\n";
+
+const char *json_example = "{\n"
+			   "  \"test\": {\n"
+			   "    \"hallo\": 1\n"
+			   "  },\n"
+			   "  \"liste\": [\n"
+			   "    1.1000000000000001,\n"
+			   "    2,\n"
+			   "    3,\n"
+			   "    4,\n"
+			   "    {\n"
+			   "      \"objekt\": {\n"
+			   "        \"key\": \"value\"\n"
+			   "      }\n"
+			   "    }\n"
+			   "  ]\n"
+			   "}";
+
+Test(utils, config_to_json)
+{
+	int ret;
+	config_t cfg;
+	config_setting_t *cfg_root;
+	json_t *json;
+	
+	config_init(&cfg);
+	
+	ret = config_read_string(&cfg, cfg_example);
+	cr_assert_eq(ret, CONFIG_TRUE);
+	
+	cfg_root = config_root_setting(&cfg);
+	
+	json = config_to_json(cfg_root);
+	cr_assert_not_null(json);
+	
+	char *str = json_dumps(json, JSON_INDENT(2));
+	
+	//printf("%s\n", str);
+	
+	json_decref(json);
+	
+	cr_assert_str_eq(str, json_example);
+	
+	config_destroy(&cfg);
+}
+
+Test(utils, json_to_config)
+{
+	config_t cfg;
+	config_setting_t *cfg_root;
+	json_t *json;
+	
+	/* For config_write() */
+	FILE *f;
+	char str[1024];
+	
+	config_init(&cfg);
+
+	cfg_root = config_root_setting(&cfg);
+	
+	json = json_loads(json_example, 0, NULL);
+	cr_assert_not_null(json);
+	
+	json_to_config(json, cfg_root);
+	
+	//config_write(&cfg, stdout);
+
+	f = fmemopen(str, sizeof(str), "w+");
+	config_write(&cfg, f);
+	fclose(f);
+	
+	cr_assert_str_eq(str, cfg_example);
+	
+	json_decref(json);
+}
---
+++ tools/conf2json.c
@@ -0,0 +1,68 @@
+/** Convert old style config to new JSON format.
+ *
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @copyright 2017, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU Lesser General Public License v2.1
+ *
+ * VILLASnode - connecting real-time simulation equipment
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *********************************************************************************/
+
+#include <jansson.h>
+#include <libconfig.h>
+
+#include <villas/utils.h>
+
+void usage()
+{
+	printf("Usage: conf2json < input.conf > output.json\n\n");
+
+	print_copyright();
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	config_t cfg;
+	config_setting_t *cfg_root;
+	json_t *json;
+	
+	if (argc != 1) {
+		usage();
+		exit(EXIT_FAILURE);
+	}
+	
+	config_init(&cfg);
+	
+	ret = config_read(&cfg, stdin);
+	if (ret != CONFIG_TRUE)
+		return ret;
+	
+	cfg_root = config_root_setting(&cfg);
+	
+	json = config_to_json(cfg_root);
+	if (!json)
+		return -1;
+	
+	ret = json_dumpf(json, stdout, JSON_INDENT(2)); fflush(stdout);
+	if (ret)
+		return ret;
+	
+	json_decref(json);
+	config_destroy(&cfg);
+	
+	return 0;
+}
