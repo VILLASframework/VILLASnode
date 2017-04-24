c02dc3b131b7734440e504d0eef7eb6ec19b19ee
Steffen Vogel <post@steffenvogel.de>
2017-04-24 11:33:26 +0200

    updated copyright headers of AsyncIP code

--- clients/opal/udp/models/send_receive/src/main.c
+++ clients/opal/udp/models/send_receive/src/main.c
@@ -1,16 +1,26 @@
-/** Main
+/** Main routine of AsyncIP.
  *
- *  Code example of an asynchronous program.  This program is started
- *  by the asynchronous controller and demonstrates how to send and 
- *  receive data to and from the asynchronous icons and a UDP or TCP
- *  port.
- *
- * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
- * @author Mathieu Dubé-Dallaire
- * @copyright 2014, Institute for Automation of Complex Power Systems, EONERC
- * @copyright 2003, OPAL-RT Technologies inc
  * @file
- */
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @copyright 2017, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU Lesser General Public License v2.1
+ *
+ * VILLASnode - connecting real-time simulation equipment
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ *********************************************************************************/
 
 /* Standard ANSI C headers needed for this program */
 #include <errno.h>
--- clients/opal/udp/models/send_receive/src/socket.c
+++ clients/opal/udp/models/send_receive/src/socket.c
@@ -1,15 +1,25 @@
-/** Helper functions for socket
- *
- *  Code example of an asynchronous program.  This program is started
- *  by the asynchronous controller and demonstrates how to send and 
- *  receive data to and from the asynchronous icons and a UDP port.
+/** Helper functions for sockets.
  *
  * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
- * @author Mathieu Dubé-Dallaire
- * @copyright 2014, Institute for Automation of Complex Power Systems, EONERC
- * @copyright 2003, OPAL-RT Technologies inc
- * @file
- */
+ * @copyright 2017, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU Lesser General Public License v2.1
+ *
+ * VILLASnode - connecting real-time simulation equipment
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ *********************************************************************************/
 
 #include <errno.h>
 #include <unistd.h>
